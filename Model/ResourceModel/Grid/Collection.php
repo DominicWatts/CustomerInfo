a4826e3582fae01c0c234ba82d1488c769139654
Dominic <dominic.watts@hotmail.co.uk>
2019-09-28 20:08:09 +0100

    1.0.0 release

--- Model/ResourceModel/Grid/Collection.php
+++ Model/ResourceModel/Grid/Collection.php
@@ -4,11 +4,14 @@ namespace Xigen\CustomerInfo\Model\ResourceModel\Grid;
 
 use Magento\Framework\View\Element\UiComponent\DataProvider\SearchResult;
 
+/**
+ * Class Collection
+ * @package Xigen\CustomerInfo\Model\ResourceModel\Grid
+ */
 class Collection extends SearchResult
 {
     /**
      * Init collection select
-     *
      * @return $this
      */
     protected function _initSelect()
@@ -23,7 +26,7 @@ class Collection extends SearchResult
             'sales_order_grid.customer_id = customer_log.customer_id',
             ['max(sales_order_grid.created_at) as order_created_at']
         )
-        ->group('main_table.entity_id');
+            ->group('main_table.entity_id');
 
         $this->addFilterToMap('customer_id', 'customer_log.customer_id');
         $this->addFilterToMap('created_at', 'main_table.created_at');
